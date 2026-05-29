78eda9f9760ee32ea505f1010bcdff80431eed6f
wyyyyy04 <694904422@qq.com>
2026-05-29 18:24:28 +0800

    版权信息更新

--- src/settings.js
+++ src/settings.js
@@ -62,5 +62,5 @@ module.exports = {
   /**
    * 底部版权文本内容
    */
-  footerContent: 'Copyright © 2018-2026 RuoYi. All Rights Reserved.'
+  footerContent: 'Copyright © 2024-2027 SDP-ChroNet. All Rights Reserved.'
 }
